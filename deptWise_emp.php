278ab5a694b5766e39a84fa9ca0286065addb1ab
ckpal9727 <95668324+ckpal9727@users.noreply.github.com>
2022-01-16 19:04:52 +0530

    select dept wise emp detail

--- deptWise_emp.php
+++ deptWise_emp.php
@@ -6,10 +6,18 @@ $result=mysqli_query($con,$select)
 ?>
 <html>
     <head>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
+    <center>
+        <h1><b>Department wise Employee details</b></h1>
+</center>
+<br>
+<br>
     <form method="">
-        <select name="op" onchange="this.form.submit()">
+        <div class="container ">
+            <div class="d-flex  justify-content-center"><div>
+        <select class="btn btn-light" name="op" onchange="this.form.submit()">
         <option>select</option>
         <?php
         $select="select * from dept_master;";
@@ -26,8 +34,9 @@ $result=mysqli_query($con,$select)
         <?php
        }
         ?>
-        </select>
-        <table border="1">
+        </select></div></div><br>
+        <br>
+        <table class="table table-hover">
             <tr>
                 <th>department id</th>
                 <th>employee id</th>
@@ -61,6 +70,11 @@ if(isset($_GET['op'])) {
 
             
     </table>
+</div>
 </form>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
 </body>
 </html>
